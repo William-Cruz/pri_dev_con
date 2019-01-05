528500c48fcf9dec6df6a7a883e622ddeb6a843a
William-Cruz <chiefxcruz@gmail.com>
2019-01-04 21:49:37 -0600

    installed express

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://Chief:4317cruz@ds149034.mlab.com:49034/privatedevconnector'
+};
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDb
+mongoose.connect(db, {
+    useNewUrlParser: true
+  })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
